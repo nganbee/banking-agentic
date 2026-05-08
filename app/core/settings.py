e00e34faf95632977dabadfffdc1a42fbaae3578
nganbee <ngankimm510@gmail.com>
2026-05-08 19:34:16 +0700

    fix: config ollama url

--- app/core/settings.py
+++ app/core/settings.py
@@ -1,9 +1,11 @@
 from pydantic_settings import BaseSettings
 from pathlib import Path
 
+_BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
 class Settings(BaseSettings):
     # Ollama Configuration
-    OLLAMA_BASE_URL: str = "http://gsvhu-34-147-68-150.run.pinggy-free.link"
+    OLLAMA_BASE_URL: str
     
     # Models
     GENERATION_MODEL: str = "gpt-oss:20b" 
@@ -13,13 +15,13 @@ class Settings(BaseSettings):
     INTENT_MODEL_PATH: str = "imbee510/bank-intent-qwen-unsloth"
 
     # Project Paths
-    BASE_DIR: Path = Path(__file__).resolve().parent.parent.parent
+    BASE_DIR: Path = _BASE_DIR
     
     # App Settings
     APP_NAME: str = "Banking Agentic"
     DEBUG: bool = True
 
     class Config:
-        env_file = ".env"
+        env_file = _BASE_DIR / ".env"
         
 settings = Settings()
